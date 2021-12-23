c03ec4dbd707b9f03897f79a245a4f51b3c8af05
Juju <juhye1215@gmail.com>
2021-12-22 21:22:07 -1000

    test

---
+++ src/components/SectionLocations/SectionItem.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from '../../util/reactIntl';
+import classNames from 'classnames';
+import { lazyLoadWithDimensions } from '../../util/contextHelpers';
+
+import { NamedLink } from '..';
+
+import css from './SectionLocations.module.css';
+
+import HnlImg from './images/honolulu.jpg';
+import MauiImg from './images/maui.jpg';
+import BigislandImge from './images/bigisland.jpg';
+
+class LocationImage extends Component {
+  render() {
+    const { alt, ...rest } = this.props;
+    return <img alt={alt} {...rest} />;
+  }
+}
+const LazyImage = lazyLoadWithDimensions(LocationImage);
+
+const locationLink = (name, image, searchQuery) => {
+  const nameText = <span className={css.locationName}>{name}</span>;
+  return (
+    <NamedLink name="SearchPage" to={{ search: searchQuery }} className={css.location}>
+      <div className={css.imageWrapper}>
+        <div className={css.aspectWrapper}>
+          <LazyImage src={image} alt={name} className={css.locationImage} />
+        </div>
+      </div>
+      <div className={css.linkText}>
+        <FormattedMessage
+          id="SectionLocations.listingsInLocation"
+          values={{ location: nameText }}
+        />
+      </div>
+    </NamedLink>
+  );
+};
+
+const SectionItem = props => {
+  const { rootClassName, className } = props;
+
+  const classes = classNames(rootClassName || css.root, className);
+
+  return (
+    <div className={classes}>
+      <div className={css.title}>
+        <FormattedMessage id="SectionLocations.title" />
+      </div>
+      <div className={css.locations}>
+        {locationLink(
+          'Honolulu',
+          HnlImg,
+          ''
+        )}
+        {locationLink(
+          'Maui',
+          MauiImg, 
+          '?address=Rovaniemi%2C%20Finland&bounds=67.18452510000002%2C27.32667850000007%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
+        )}
+        {locationLink(
+          'Big Island',
+          BigislandImge,
+          '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
+        )}
+      </div>
+    </div>
+  );
+};
+
+SectionItem.defaultProps = { rootClassName: null, className: null };
+
+const { string } = PropTypes;
+
+SectionItem.propTypes = {
+  rootClassName: string,
+  className: string,
+};
+
+export default SectionItem;
---
+++ src/containers/LandingPage/ImageData.js
@@ -0,0 +1,62 @@
+
+  [
+      {
+        img:
+          "/static/media/outdoor.png  ",
+        cols: 1,
+        title: 'First Image Title',
+        overlay: 'blue-strong'
+      },
+      {
+        img:
+          './oceanfront.png',
+        cols: 2,
+        title: 'Ocean Front',
+        overlay: 'blue-light'
+      },
+      {
+        img:
+          './outdoor.png',
+        cols: 1,
+        title: 'Outdoor',
+        overlay: 'blue-slight'
+      },
+      {
+        img:
+          './chapel.png',
+        cols: 2,
+        title: 'Chapel',
+        overlay: 'lime-strong'
+      },
+      {
+        img:
+          'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(75).webp',
+        cols: 2,
+        title: 'image',
+        overlay: 'lime-light'
+      },
+  
+      {
+        img:
+          'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).webp',
+        cols: 1,
+        title: 'image',
+        overlay: 'lime-slight'
+      },
+      {
+        img:
+          'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(77).webp',
+        cols: 2,
+        title: 'image',
+        overlay: 'teal-strong'
+      },
+      {
+        img:
+          'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).webp',
+        cols: 1,
+        title: 'image',
+        overlay: 'teal-light'
+      }
+    ];
+
+    export default ImageData;
